858c71a2ec7c1af8b9e9985fe20a4191536481c6
Zach King <zsking74@gmail.com>
2023-10-09 10:40:56 -0400

    Added annotations to Rating class

--- Data/Rating.cs
+++ Data/Rating.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,15 @@ namespace RestaurantRaterAPI.Data
 {
     public class Rating
     {
+        [Key]
         public int Id {get; set;}
+
+        [Required]
+        [Range(0, 5)]
         public double Score { get; set; }
+
+        [Required]
+        [ForeignKey("Restaurant")]
         public int RestaurantId { get; set; }
     }
 }
