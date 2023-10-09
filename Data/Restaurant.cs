dea2cfc549fa7daa0cb893e1e918d9d360d4bf10
Zach King <zsking74@gmail.com>
2023-10-09 10:36:46 -0400

    Added Annotations to the class Restaurant

--- Data/Restaurant.cs
+++ Data/Restaurant.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,13 @@ namespace RestaurantRaterAPI.Data
 {
     public class Restaurant
     {
+        [Key]
         public int Id {get; set;}
+
+        [Required, MaxLength(100)]
         public string Name { get; set; } = string.Empty;
+
+        [Required, MaxLength(100)]
         public string Location { get; set; } = string.Empty;
     }
 }
